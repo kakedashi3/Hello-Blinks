e0e4953da1e0978d51191f5910586ee73113bafa
tatsuoka <69132449+Pabulow2060@users.noreply.github.com>
2024-07-18 02:52:42 +0900

    add kakedashi

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  metadataBase: new URL(``),
+  metadataBase: new URL(`https://verylonganimals.com/`),
   title: {
     default: "Kakedashi",
     template: `%s Hello`,
